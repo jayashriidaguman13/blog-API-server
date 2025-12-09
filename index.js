1d5e9519689e2b085403e217cfe9bc30606e5de9
jsdaguman <jayashrii.daguman13@gmail.com>
2025-12-09 23:19:54 +0800

    Fixed CORS

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://blog-api-self-gamma.vercel.app/"
+  "https://blog-api-self-gamma.vercel.app"
 ];
 
 app.use(cors({
